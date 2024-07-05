0a9e162a522c5753ddb7ad1c5cc22d5528f3defa
Justin Lefrançois <justin.lefrancois@polymtl.ca>
2024-07-04 20:50:05 -0400

    Initial Commit

---
+++ LaserSync/LaserSyncApp.swift
@@ -0,0 +1,17 @@
+//
+//  LaserSyncApp.swift
+//  LaserSync
+//
+//  Created by Justin Lefrançois on 2024-07-04.
+//
+
+import SwiftUI
+
+@main
+struct LaserSyncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
